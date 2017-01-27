39745af3fb8b384d38cd18ce89f753bc15d87726
Greg Robison <gjrobison64@gmail.com>
2017-01-27 17:36:15 -0500

    work in progress

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
 var randWord = commonWords[Math.floor(Math.random() * commonWords.length)];
 console.log(randWord);
 
 for (var i = 0; i < randWord.length; i++) {
-  var char =document.createElement('span');
+  var char = document.createElement('span');
+  char.id = "letter-" + i;
   char.textContent = ' _';
   document.querySelector(".spaces").appendChild(char);
+}
+
+var checkGuess = function functionName(event){
+for (var i = 0; i < randWord.length; i++) {
+  if(randWord[i] == event.target.textContent){
+    document.querySelector("#letter-" + i).textContent = randWord[i];
+  }
+else {
+  querySelector("#letter-" + i).disabled = true;
+}
+
+
+}
+}
+
+
+for (var i = 0; i < alphabet.length; i++) {
+  var letter = document.createElement('button');
+  letter.textContent = alphabet[i];
+  document.querySelector(".letters").appendChild(letter);
+  letter.addEventListener("click", checkGuess)
 }
